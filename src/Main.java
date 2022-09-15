93044b04ba0c694aef1ff05c8e0482abaad9e672
ViktoriiaAngelovskaya <vshilova3@gmail.com>
2022-09-15 19:10:16 +0300

    done

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(in);
+        while (true) {
+            out.println("Выберите пункт");
+            out.println("1.Сложение");
+            out.println("2.Вычитание");
+            out.println("3.Умножение");
+            out.println("4.Деление");
+            out.println("5.Выйти");
+            int person = scanner.nextInt();
+            int result;
+            if (person == 5) {
+                break;
+            } else if (person == 1) {
+                System.out.println("Введите первое число:");
+                int a = scanner.nextInt();
+                System.out.println("Введите второе число:");
+                int b = scanner.nextInt();
+                System.out.println("a : " + a);
+                System.out.println("b : " + b);
+                result = a + b;
+                System.out.println("Ответ: " + result);
+            } else if (person == 2) {
+                System.out.println("Введите первое число:");
+                int a = scanner.nextInt();
+                System.out.println("Введите второе число:");
+                int b = scanner.nextInt();
+                System.out.println("a : " + a);
+                System.out.println("b : " + b);
+                result = a - b;
+                System.out.println("Ответ: " + result);
+            } else if (person == 3) {
+                System.out.println("Введите первое число:");
+                int a = scanner.nextInt();
+                System.out.println("Введите второе число:");
+                int b = scanner.nextInt();
+                System.out.println("a : " + a);
+                System.out.println("b : " + b);
+                result = a * b;
+                System.out.println("Ответ: " + result);
+
+            } else if (person == 4) {
+                System.out.println("Введите первое число:");
+                int a = scanner.nextInt();
+                System.out.println("Введите второе число:");
+                int b = scanner.nextInt();
+                if (b == 0) {
+                    System.out.println("На ноль делить нельзя");
+                }
+                System.out.println("a : " + a);
+                System.out.println("b : " + b);
+                result = a / b;
+                System.out.println("Ответ: " + result);
+            } else {
+                System.out.println("Ошибка/Такой операции не существует");
+            }
+        }
+        System.out.println("Программа завершена");
+    }
+}
+
